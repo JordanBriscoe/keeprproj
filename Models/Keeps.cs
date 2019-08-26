ca9e4e592e868ce66e2b5288355cbf65c13028d3
JordanBriscoe <briscoe41@gmail.com>
2019-08-26 09:30:19 -0600

    No Errors Models 1/2 done

---
+++ Models/Keeps.cs
@@ -0,0 +1,10 @@
+
+
+namespace Keepr.Models
+{
+  public class Keeps
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+  }
+}
---
+++ Models/Vault.cs
@@ -0,0 +1,10 @@
+
+
+namespace Keepr.Models
+{
+  public class Vault
+  {
+    public string name { get; set; }
+    public string description { get; set; }
+  }
+}
